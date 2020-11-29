0510d5242d3a0299fe1f7cecd66e9f6cc7252886
Kskulski <firestar345@gmail.com>
2020-11-29 12:53:21 -0600

    adding item to list TaskMaster js

--- TaskMaster.js
+++ TaskMaster.js
@@ -1,5 +1,28 @@
 function addItem() {
+    var list = document.createElement("li");
+    var newInput = document.getElementById("newInput").value;
+    var text = document.createTextNode(newInput);
+    list.appendChild(text);
+    if (newInput == "") {
+        alert("Write something first!");
+    } else {
+        document.getElementById("itemUL").appendChild("list");
+    }
+    document.getElementById("newInput").value = "";
 
+    var close = document.createElement("SPAN");
+    var text = document.createTextNode("\u00D7");
+    span.className = "x";
+    x.appendChild(text);
+    list.appendChild(close);
+
+    //fix?
+    for (i = 0; i < x.length; i++) {
+        x[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
 
 function importList() {
@@ -14,4 +37,7 @@ function save() {
 
 }
 
+function newList() {
+    
+}
 //onload?
